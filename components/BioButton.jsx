12a3d658afbb4a48453303ec04ab7a88b00f7ae7
Colton Randall <coltonrandall.nz@gmail.com>
2025-02-16 09:28:29 +1300

    Finish components

---
+++ components/BioButton.jsx
@@ -0,0 +1,13 @@
+import "./BioButton.css";
+
+function BioButton() {
+  return (
+    <div id="buttonDiv">
+      <button type="button" onclick="showAboutme()">
+        Get to know me!
+      </button>
+    </div>
+  );
+}
+
+export default BioButton;
---
+++ components/UpArrow.jsx
@@ -0,0 +1,13 @@
+import "./UpArrow.css";
+
+function UpArrow() {
+  return (
+    <div className="toTop">
+      <a href="#">
+        <img src="./images/up.png" id="arrow" title="Back to top!" />
+      </a>
+    </div>
+  );
+}
+
+export default UpArrow;
