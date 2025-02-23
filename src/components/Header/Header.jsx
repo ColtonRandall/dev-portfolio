0f1cb75cc622716b7dcd753e5c233d9aecf2a1e7
Colton Randall <coltonrandall.nz@gmail.com>
2025-02-23 14:11:16 +1300

    tidy up responsive design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,25 +2,23 @@ import "./Header.css";
 
 function Header() {
   return (
-    <>
-      <div id="header">
-        <h1 id="title">
-          Colton
-          <br />
-          Randall
-        </h1>
-        <img src="./images/me.jpeg" id="mainphoto" title="Me 🙂" />
+    <div id="header">
+      <h1 id="title">
+        Colton
+        <br />
+        Randall
+      </h1>
+      <img src="./images/me.jpeg" id="mainphoto" title="Me 🙂" />
 
-        <div id="subTitle">
-          <div id="SEtitle">
-            Software Engineer <blink>|</blink>
-          </div>
-          <br />
-          with a background in Aviation and Biochemistry
+      <div id="subTitle">
+        <div id="SEtitle">
+          Software Engineer <blink>|</blink>
         </div>
-        <p id="location"> 📍 Auckland | New Zealand</p>
+        <br />
+        with a background in Aviation and Biochemistry
       </div>
-    </>
+      <p id="location"> 📍 Auckland | New Zealand</p>
+    </div>
   );
 }
 
