1be886af0a10e54da0ec66569bf90f8acb7941dc
Colton Randall <coltonrandall.nz@gmail.com>
2025-03-02 12:21:07 +1300

    rename to dev portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/colton-website-react/">
+    <BrowserRouter basename="/dev-portfolio/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
