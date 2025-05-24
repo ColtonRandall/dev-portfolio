9fa7e63c67e9cda3e32eefd58f553238ac138044
Colton Randall <coltonrandall.nz@gmail.com>
2025-05-24 16:33:02 +1200

    add blurb on home page

---
+++ src/components/HomeContent/HomeContent.jsx
@@ -0,0 +1,33 @@
+import "./HomeContent.css";
+
+function HomeContent() {
+  return (
+    <div className="home-content">
+      <p>
+        <div style={{ textAlign: "center" }}>
+          🛠️ <br />
+        </div>
+        Full stack engineer with strong experience across both front and back
+        end, specialising in C# and React, and currently working with Java 17
+        and Spring Boot.
+        <br />
+        <br />
+        <div style={{ textAlign: "center" }}>
+          📚 <br />
+        </div>
+        Passionate about continuous learning and applying it to build scalable,
+        user-friendly solutions with a focus on clean, maintainable code.
+        <br />
+        <br />
+        <div style={{ textAlign: "center" }}>
+          ☀️ <br />
+        </div>
+        Outside of work, I enjoy side projects (this portfolio being one of
+        them), photography, travel, staying active, and exploring the outdoors
+        with my wife and golden retriever.
+      </p>
+    </div>
+  );
+}
+
+export default HomeContent;
