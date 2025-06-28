c633104b5d0385de6ccf7c6993b6107c7076aa9f
Colton Randall <coltonrandall.nz@gmail.com>
2025-06-28 18:09:13 +1200

    update media query for icons

--- src/components/Icons/Icons.jsx
+++ src/components/Icons/Icons.jsx
@@ -6,14 +6,14 @@ import DescriptionIcon from "@mui/icons-material/Description";
 import { Tooltip } from "@mui/material";
 import PropTypes from "prop-types";
 
-function Icons({ size = 27, iconColor = "#6A9F85" }) {
+function Icons({iconColor = "#6A9F85" }) {
   return (
     <div className="icons">
       <Tooltip title="My CV" placement="bottom">
         <a href="./docs/Colton CV 2025.pdf">
           <DescriptionIcon
-            className="icon cvicon"
-            style={{ fontSize: size, color: iconColor }}
+            className="cvicon"
+            style={{color: iconColor }}
           />
         </a>
       </Tooltip>
@@ -21,8 +21,8 @@ function Icons({ size = 27, iconColor = "#6A9F85" }) {
       <Tooltip title="My GitHub profile" placement="bottom">
         <a href="https://github.com/ColtonRandall">
           <GitHubIcon
-            className="icon githubicon"
-            style={{ fontSize: size, color: iconColor }}
+            className="githubicon"
+            style={{color: iconColor }}
           />
         </a>
       </Tooltip>
@@ -30,8 +30,8 @@ function Icons({ size = 27, iconColor = "#6A9F85" }) {
       <Tooltip title="My LinkedIn page" placement="bottom">
         <a href="https://www.linkedin.com/in/coltonrandall/">
           <LinkedInIcon
-            className="icon linkedinicon"
-            style={{ fontSize: size + 2, color: iconColor }}
+            className="linkedinicon"
+            style={{color: iconColor }}
           />
         </a>
       </Tooltip>
@@ -39,8 +39,8 @@ function Icons({ size = 27, iconColor = "#6A9F85" }) {
       <Tooltip title="Flick me an email" placement="bottom">
         <a href="mailto:coltonrandall.nz@gmail.com">
           <EmailIcon
-            className="icon emailicon"
-            style={{ fontSize: size + 2, color: iconColor }}
+            className="emailicon"
+            style={{color: iconColor }}
           />
         </a>
       </Tooltip>
