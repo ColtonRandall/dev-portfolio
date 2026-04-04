2439d60e521f0ee0345b008f2130aeb4be90ab79
Colton Randall <coltonrandall.nz@gmail.com>
2026-04-05 09:37:52 +1200

    add fade in when changing pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Certificates from "./pages/Certificates/Certificates";
 import Education from "./pages/Education/Education";
@@ -8,19 +8,26 @@ import Blog from "./pages/Blog/Blog";
 import ColtonTimeline from "./pages/Timeline/ColtonTimeline";
 import BlogPostPage from "./pages/Blog/BlogPostPage";
 
-function App() {
+function AnimatedRoutes() {
+  const location = useLocation();
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/timeline" element={<ColtonTimeline />} />
-      <Route path="/education" element={<Education />} />
-      <Route path="/projects" element={<Projects />}></Route>
-      <Route path="/certificates" element={<Certificates />} />
-      <Route path="/blog" element={<Blog />} />
-      <Route path="/fizzbuzz" element={<FizzBuzz />}></Route>
-      <Route path="/blog/:slug" element={<BlogPostPage />} />
-    </Routes>
+    <div key={location.pathname} className="page-fade">
+      <Routes location={location}>
+        <Route path="/" element={<Home />} />
+        <Route path="/timeline" element={<ColtonTimeline />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/certificates" element={<Certificates />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/fizzbuzz" element={<FizzBuzz />} />
+        <Route path="/blog/:slug" element={<BlogPostPage />} />
+      </Routes>
+    </div>
   );
+}
+
+function App() {
+  return <AnimatedRoutes />;
 }
 
 export default App;
